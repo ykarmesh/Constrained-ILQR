4c8c1cf71fb84ef51d7c3ab404873ba63ac4ac6d
pparmesh <pparmesh@andrew.cmu.edu>
2019-11-19 16:40:43 -0500

    Local Planner

--- scripts/ilqr/local_planner.py
+++ scripts/ilqr/local_planner.py
@@ -5,11 +5,14 @@ class LocalPlanner:
     Class which creates a desired trajectory based on the global plan
     Created by iLQR class based on the plan provided by the simulator
     """
-    def __init__(self, state, control, args):
+    def __init__(self, args):
         self.args = args
         self.global_plan = None
-        self.state = state
+        self.state = None
         self.control = control
+    
+    def set_current_state(self, ego_state):
+        self.ego_state = ego_state
 
     def set_global_planner(self, global_plan):
         """
@@ -26,12 +29,14 @@ class LocalPlanner:
         """
         Creates a local plan based on the waypoints on the global planner 
         """
+        assert ego_state is not None, "Ego state was not set in the LocalPlanner"
+
         # Find index of waypoint closest to current pose
         closest_ind = self.closest_node([self.state[0],self.state[1]], global_wpts) 
         local_wpts = [[global_wpts[i,0],global_wpts[i,1]] for i in range(closest_ind, closest_ind + self.args.number_of_local_wpts)]
         return local_wpts
 
-    def local_plan_function(self, global_wpts)
+    def local_plan_function(self, global_wpts):
         local_wpts = get_local_plan_wpts(global_wpts)
         x = local_wpts[:,0]
         y = local_wpts[:,1]
--- scripts/simulator/navigation_agent_CARLA.py
+++ scripts/simulator/navigation_agent_CARLA.py
@@ -27,7 +27,7 @@ if python_version.startswith('3'):
 try:
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 except IndexError:
-    pass
+    print("Cannot add the common path {}".format(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 import carla
 from agents.navigation.basic_agent import BasicAgent
@@ -37,12 +37,12 @@ from agents.navigation.local_planner import RoadOption
 from route_manipulation import interpolate_trajectory
 
 from arguments import add_arguments
-from scripts.iLQR import iLQR
+from ilqr.iLQR import iLQR
 
 class Carla_Interface():
-    def __init__(self, args, town='Town01', verbose=False):
+    def __init__(self, args, town='Town01'):
         self.args = args
-        self.verbose = verbose
+        self.verbose = args.verbose
         self.client = carla.Client('127.0.0.1', 2000)
         self.client.set_timeout(10.0)
 
@@ -183,8 +183,8 @@ class Carla_Interface():
         assert self.navigation_agent != None, "Navigation Agent not initialized"
 
         while True:
-            control = self.navigation_agent.run_step()
-            self.ego_vehicle.apply_control(control)
+            control = self.navigation_agent.run_step(self.get_ego_states(), self.get_npc_state())
+            # self.ego_vehicle.apply_control(control)
 
 
     def spawn_npc(self):
@@ -263,10 +263,14 @@ if __name__ == "__main__":
         argparser = argparse.ArgumentParser(description='CARLA CILQR')
         add_arguments(argparser)
         args = argparser.parse_args()
-        pdb.set_trace()
         carla_interface = Carla_Interface(args)
-        carla_interface.create_pid_agent()
-        carla_interface.run_step_pid()
+        if args.use_pid:
+            carla_interface.create_pid_agent()
+            carla_interface.run_step_pid()
+        else:
+            carla_interface.create_ilqr_agent()
+            carla_interface.run_step_ilqr()
+
     except KeyboardInterrupt:
         pass
     finally:
